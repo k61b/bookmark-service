f853a6caec58e3fb2651b559a732f32cd7947fd5
Kayra Berk Tuncer <kayrabtuncer@gmail.com>
2022-06-19 18:08:53 +0300

    chore: add auth guard

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Request } from 'express';
+import { JwtGuard } from 'src/auth/guard';
 
 @Controller('users')
 export class UserController {
+  @UseGuards(JwtGuard)
   @Get('me')
-  getMe() {
-    return 'user info';
+  getMe(@Req() req: Request) {
+    return req.user;
   }
 }
