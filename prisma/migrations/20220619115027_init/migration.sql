1528faae8461e300f7e3267e6f8d540700446363
Kayra Berk Tuncer <kayrabtuncer@gmail.com>
2022-06-19 15:10:43 +0300

    chore: automate postgres restart and prisma migrations

--- prisma/migrations/20220619115027_init/migration.sql
+++ prisma/migrations/20220619115027_init/migration.sql
@@ -1,27 +1,31 @@
 -- CreateTable
-CREATE TABLE "User" (
+CREATE TABLE "users" (
     "id" SERIAL NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
-    "emial" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
     "hash" TEXT NOT NULL,
     "firstName" TEXT,
     "lastName" TEXT,
 
-    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
-CREATE TABLE "Bookmarl" (
+CREATE TABLE "bookmarks" (
     "id" SERIAL NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
     "title" TEXT NOT NULL,
     "description" TEXT,
     "link" TEXT NOT NULL,
+    "userId" INTEGER NOT NULL,
 
-    CONSTRAINT "Bookmarl_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "bookmarks_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "User_emial_key" ON "User"("emial");
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- AddForeignKey
+ALTER TABLE "bookmarks" ADD CONSTRAINT "bookmarks_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
