e8547eea2208f68a47dc9b9e403e2ded3547a1bf
Kayra Berk Tuncer <kayrabtuncer@gmail.com>
2022-07-02 14:05:25 +0300

    chore: prisma database treadown logic

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,9 +1,11 @@
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
+import { PrismaService } from '../src/prisma/prisma.service';
 import { AppModule } from '../src/app.module';
 
 describe('App e2e test', () => {
   let app: INestApplication;
+  let prisma: PrismaService;
 
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
@@ -16,9 +18,40 @@ describe('App e2e test', () => {
       }),
     );
     await app.init();
+
+    prisma = app.get(PrismaService);
+
+    await prisma.cleanDb();
   });
-  it.todo('shold test');
   afterAll(async () => {
     app.close();
+  });
+
+  describe('Auth', () => {
+    describe('Signup', () => {
+      it.todo('should signup');
+    });
+
+    describe('Signin', () => {
+      it.todo('should signin');
+    });
+  });
+
+  describe('User', () => {
+    describe('Get me', () => {});
+
+    describe('Edit user', () => {});
+  });
+
+  describe('Bookmarks', () => {
+    describe('Get bookmarks', () => {});
+
+    describe('Create bookmark', () => {});
+
+    describe('Get bookmark by Id', () => {});
+
+    describe('Edit bookmark', () => {});
+
+    describe('Delete bookmark', () => {});
   });
 });
